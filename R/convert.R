b1b0b040e09a2254407d5b98e4b444d1b991ec59
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-07-23 09:36:10 +1000

    feat: export working convert() with vignette demo
    - Add dplyr transformation steps to convert()
    - Add check convert() & script output match
    - Add %||% null case operator

--- R/convert.R
+++ R/convert.R
@@ -17,36 +17,48 @@
 #' @param values_from A string specifying which column in `data` to get cell values from.
 #' @param values_to A string specifying the name of the new column created to store
 #' the distributed values. Defaults to "value_new"
-#' @param weights A string specifying which column in `code_dict` to get the
+#' @param weight_col A string specifying which column in `code_dict` to get the
 #' weights to distribute `values_from` between `code_from` and `code_to`
 #' @return
 #' @export
 #'
+#' @importFrom assertthat assert_that
 #' @examples
-convert <- function(data, code_dict, code_from, code_to, values_from, values_to = NULL, weights){
+#'
+#'
+convert <- function(data, code_dict, code_from, code_to, values_from, values_to = NULL, weight_col){
   # ---- input existence checks ----
   ### code_from exists in both data & code_dict
-  assertthat::assert_that(has_name(data, code_from))
-  assertthat::assert_that(has_name(code_dict, code_from))
+  assert_that(assertthat::has_name(data, code_from))
+  assert_that(assertthat::has_name(code_dict, code_from))
   ### code_to exists in code_dict
-  assertthat::assert_that(has_name(code_dict, code_to))
+  assert_that(assertthat::has_name(code_dict, code_to))
 
   ### values_from exists in data
-  assertthat::assert_that(has_name(data, values_from))
+  assert_that(assertthat::has_name(data, values_from))
   ### values_to is given or created
   values_to %||% "value_new"
   ### weights are provided
-  assertthat::assert_that(has_name(code_dict, weights))
+  assert_that(assertthat::has_name(code_dict, weight_col))
 
   # ---- code_dict checks ----
   ### no duplicate instruction in code_dict // probably safe to correct inside fnc
-  assertthat::assert_that(nrow(code_dict) == nrow(distinct(code_dict)))
+  assert_that(nrow(code_dict) == nrow(dplyr::distinct(code_dict)))
   ### every code in data has a destination instruction in code_dict
-  assertthat::assert_that(all(unique(data[code_from]) %in% unique(code_dict[code_from])))
+  assert_that(all(unique(data[code_from]) %in% unique(code_dict[code_from])))
   ### value distribution weights total exactly 1 for each code_from (no value loss or creation)
   t_weight_by_code_from <- code_dict %>%
-    dplyr::group_by(!!sym(code_from)) %>%
-    dplyr::summarise(t_weight = sum(!!sym(weights)))
+    dplyr::group_by(!!rlang::sym(code_from)) %>%
+    dplyr::summarise(t_weight = sum(!!rlang::sym(weight_col)))
   assertthat::assert_that(all(t_weight_by_code_from$t_weight == 1))
 
+  # ---- transform data ----
+  data_to <- dplyr::right_join(x = data,
+                              y = code_dict,
+                        by = code_from) %>%
+    dplyr::mutate(weight_value = !!rlang::sym(weight_col) * !!rlang::sym(values_from)) %>%
+    dplyr::group_by(!!rlang::sym(code_to), .add = TRUE) %>%
+    dplyr::summarise({{values_to}} := sum(weight_value))
+
+  return(data_to)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,3 @@
+
+
 `%||%` <- function(x, y) if (is.null(x)) y else x
