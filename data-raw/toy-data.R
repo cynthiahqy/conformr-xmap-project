9948b029f9baf14c944c207ff4a7ba8412647959
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-07-23 13:22:30 +1000

    test: build bad variables in toy data
    - add bad non-numeric value column
    - add bad weights

--- data-raw/toy-data.R
+++ data-raw/toy-data.R
@@ -6,17 +6,21 @@ library(dplyr)
 ## [x] data_in: include case variable
 
 # BREAKING CASES
-## [] data_in: include BAD non-numeric value column
-## [] code_dict: include BAD weights -- sum > 1
-## [] code_dict: remove mapping for one origin code
-## [] code_dict: include duplicate mapping
+## [x] data_in: include BAD non-numeric value column
+## [x] code_dict: include BAD weights -- sum > 1
+## [x] code_dict: include BAD weights -- sum < 1?
+## [>] code_dict: remove mapping for one origin code in TEST
+## [>] code_dict: include duplicate mapping in TEST
 
-origin_codes <- c("x001", "x002", "x003", "x004")
+codeA <- c("x001", "x002", "x003", "x004", "x005")
+n_codeA <- length(codeA)
 cases <- list(
-  ABC = dplyr::tibble(code_A = origin_codes,
-               value_A = rep_len(10, length(origin_codes))),
-  XYZ = dplyr::tibble(code_A = origin_codes,
-               value_A = rep_len(100, length(origin_codes)))
+  ABC = dplyr::tibble(code_A = codeA,
+               value_A = rep_len(10, n_codeA),
+               value_bad = stringi::stri_rand_strings(n_codeA, 5)),
+  XYZ = dplyr::tibble(code_A = codeA,
+               value_A = rep_len(1000, n_codeA),
+               value_bad = stringi::stri_rand_strings(n_codeA, 5))
 )
 
 data_A <- cases %>% dplyr::bind_rows(.id = "case")
@@ -26,18 +30,31 @@ codes_BA <- dplyr::tribble(~ code_B, ~ code_A,
                                 "A1", "x001", # one-to-one
                                 "B2", "x002", # many-to-one
                                 "B2", "x003",
-                                "C3", "x004", # one-to-many
+                                "C3", "x004", # one-to-many (4)
                                 "C4", "x004",
                                 "C5", "x004",
-                                "C6", "x004"
+                                "C6", "x004",
+                                "C7", "x005", # one-to-many (3)
+                                "C8", "x005",
+                                "C9", "x005"
                                 )
 
-## generate default weights
+## generate code_dict with weights
+eps <- 0.001 # small "dust" to mess up weights
+
 code_dict_BA <- codes_BA %>%
     dplyr::group_by(code_A) %>%
     dplyr::mutate(n_dest = n_distinct(code_B),
            weight = 1 / n_dest) %>%
-    dplyr::ungroup()
+    dplyr::ungroup() %>%
+  # add bad weights
+    dplyr::mutate(weight_more = case_when(weight == 1 ~ weight,
+                                         TRUE ~ weight + eps),
+                  weight_less = case_when(n_dest == 3 ~ 0.33,
+                                          n_dest == 4 ~ 0.2,
+                                          TRUE ~ weight)
+                  )
+
 
 ## intermediate merge of code & value transfer weights
 AB_merged <- data_A %>%
@@ -58,4 +75,4 @@ toy_AB <- list("data_in" = data_A,
                 "df_merged" = AB_merged,
                 "data_out" = data_AB_out)
 
-# usethis::use_data(toy_AB, internal = TRUE)
+usethis::use_data(toy_AB, internal = TRUE, overwrite = TRUE)
