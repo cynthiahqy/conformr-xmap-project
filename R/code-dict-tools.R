be41174c1951b76ef25f87bd5f6312d3c6e943b9
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-08-28 18:04:19 +1000

    feat: add check_cd_coverage() with test
    Add code_dict helper to check coverage of code mapping
    with basic roxygen comments

--- R/code-dict-tools.R
+++ R/code-dict-tools.R
@@ -1,3 +1,40 @@
+#' Check code-dict coverage
+#'
+#' Checks if every `code_from` in `data` has
+#' at least one destination (row) in `code_dict`.
+#' Warns if any `data$code_from` is not present in `code_dict$code_from`
+#'
+#' @param data <df> with data to be converted. Must contain `code_from` column.
+#' @param code_dict <dataframe> with instructions for `code_from` into target schema.
+#' @param code_from col containing codes to be converted
+#'
+#' @return
+#' @export
+#'
+#' @family code_dict helpers
+#'
+#' @examples
+check_cd_coverage <- function(data, code_dict, code_from){
+  enexpr <- rlang::enexpr
+  ### every code in data has a destination instruction in code_dict
+  # unique codes
+  data_codes <- sort(unique(data[[code_from]]))
+  cd_codes <- sort(unique(code_dict[[code_from]]))
+
+  # coverage checks
+  ## at least one instruction
+  covered <- data_codes %in% cd_codes
+  n_not_covered <- sum(!covered)
+
+  # messages
+  if (all(covered)){
+    rlang::inform(message = c("All unique `data$code_from` are found in `code_dict$code_from`"))
+  } else {
+    rlang::warn(message = c("Can't find some `data$code_from` in `code_dict$code_from`:")
+    )
+  }
+}
+
 check_cd_weights <- function(code_dict, code_from, code_to, weight_col){
   ### no duplicate instruction in code_dict
   assertthat::assert_that(nrow(code_dict) == nrow(dplyr::distinct(code_dict)))
@@ -6,11 +43,6 @@ check_cd_weights <- function(code_dict, code_from, code_to, weight_col){
     dplyr::group_by(!!rlang::sym(code_from)) %>%
     dplyr::summarise(t_weight = sum(!!rlang::sym(weight_col)))
   assertthat::assert_that(all(t_weight_by_code_from$t_weight == 1))
-}
-
-check_cd_coverage <- function(code_dict, data, code_from, code_to){
-  ### every code in data has a destination instruction in code_dict
-  assertthat::assert_that(all(unique(data[code_A]) %in% unique(code_dict[code_A])))
 }
 
 make_cd_equal <- function(codes, code_from, code_to){
--- tests/testthat/test-code-dict-tools.R
+++ tests/testthat/test-code-dict-tools.R
@@ -1,22 +1,31 @@
 ## tests use internal data created in data-raw/toy-data.R
 
 ## prepare data inputs
-codes <- conformr:::toy_AB$df_codes
-code_dict <- conformr:::toy_AB$code_dict
+data_in <- conformr:::toy_AB$data_in
+good_codes <- conformr:::toy_AB$df_codes
+good_dict <- conformr:::toy_AB$code_dict %>% dplyr::select(code_B, code_A, n_dest, weight)
 
-dup_codes <- rbind(codes, codes[1,]) ## make duplicate code instruction
+dup_codes <- rbind(good_codes, good_codes[1,]) ## make duplicate code mapping
+dropA_dict <- code_dict[code_dict$code_A != "x001",] ## remove code instruction
 
-## make_cd_equal
+## check_cd_coverage ----
+test_that("incomplete `code_dict` raises check_cd_coverage() warning", {
+  expect_warning(check_cd_coverage(data = data_in, code_dict = dropA_dict, code_from = "code_A"))
+})
+
+## check_cd_weights ----
+
+
+
+## make_cd_equal ----
 test_that("duplicate code instructions are caught", {
   expect_error(make_cd_equal(dup_codes, code_from = code_A, code_to = code_B))
 })
 
 test_that("make_cd_equal makes same weights as toy_AB", {
-  fnc_out <- make_cd_equal(codes, code_from = code_A, code_to = code_B)
-  compare_cd <- code_dict %>% select(code_B, code_A, n_dest, weight)
-  expect_identical(fnc_out, compare_cd)
+  fnc_out <- make_cd_equal(good_codes, code_from = code_A, code_to = code_B)
+  expect_identical(fnc_out, good_dict)
 })
 
-## check_cd_
-
 # TODO: write tests using toy_AB data
+
