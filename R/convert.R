f614f3d6fe83cc72b811b19f738809d9c4822b1f
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-09-07 14:28:39 +1000

    feat: implement data_map input for convert()
    - switch from data + code_dict input to single data_map input
    - all col arguments take var exprs, removed all string arguments
    - update ideal case test for convert()
    
    close #1, close #3

--- R/convert.R
+++ R/convert.R
@@ -2,22 +2,20 @@
 #' Convert data between standards
 #'
 #' @description
-#' `convert()` maps numeric data from one category standard into another.
-#' It maintains the total "amount" of data across one-to-one, many-to-one,
+#' `convert()` maps numeric data from one category standard into another,
+#' and verifies that total "amount" of data across one-to-one, many-to-one,
 #' and one-to-many correspondences.
 #'
 #' Learn more in `vignette("convert")`
 #'
-#' @param data A data frame to convert. Can be grouped data.
-#' @param code_dict A data frame with code correspondence between standards.
-#' @param code_from A string specifying the shared column name in `data` and `code_dict`
-#' with the codes to convert from.
-#' @param code_to A string specifying which column in `code_dict` contains the
-#' destination codes to convert to.
-#' @param values_from `<tidy-select>` columns in `data` to get cell values from.
+#' @param data_map A data frame with data to be converted,
+#' and correspondence and weights between codes, for every data value
+#' @param code_in Variable in `data_map` containing the codes to convert from
+#' @param code_out Variable in `data_map` containing the destination codes to convert to.
+#' @param values_from `<tidy-select>` columns in `data-map` to get cell values from.
 #' @param names_suffix A string appended to every `value_from` column name to create column names for transformed values.
 #' Defaults to "_out"
-#' @param weight_col A string specifying which column in `code_dict` to get the
+#' @param weights Variable in `data_map` to get the
 #' weights to distribute `values_from` between `code_from` and `code_to`
 #' @return
 #' @export
@@ -27,40 +25,18 @@
 #' @examples
 #'
 #'
-convert <- function(data, code_dict, code_from, code_to, weight_col, values_from, names_suffix = "_out"){
-  # ---- quosures ----
-  q_weight <- rlang::enquo(weight_col)
-  q_values <- rlang::enquo(values_from)
-
-  # ---- input existence checks ----
-  ### code_from exists in both data & code_dict
-  assertthat::assert_that(has_name(data, code_from))
-  assertthat::assert_that(has_name(code_dict, code_from))
-  ### code_to exists in code_dict
-  assertthat::assert_that(has_name(code_dict, code_to))
-
-  ### values_from exists in data
-  # assertthat::assert_that(has_name(data, rlang::quo_name(q_values)))
-  # assertthat::assert_that(is.numeric(data[[rlang::quo_name(q_values)]]))
-  ### weights are provided
-  assertthat::assert_that(has_name(code_dict, rlang::quo_name(q_weight)))
-
-  # ---- code_dict checks ---- //
-  # TODO: replace with code-dict-tools fncs
-  ### no duplicate instruction in code_dict // probably safe to correct inside fnc
-  assertthat::assert_that(nrow(code_dict) == nrow(dplyr::distinct(code_dict)))
-  ### every code in data has a destination instruction in code_dict
-  assertthat::assert_that(all(unique(data[code_from]) %in% unique(code_dict[code_from])))
+convert <- function(data_map, code_in, code_out, weights, values_from, names_suffix = "_out"){
+  # ---- correspondence checks ---- //
   ### value distribution weights total exactly 1 for each code_from (no value loss or creation)
-  t_weight_by_code_from <- code_dict %>%
-    dplyr::group_by(.data[[code_from]]) %>%
-    dplyr::summarise(t_weight = sum({{ weight_col }}))
-  assertthat::assert_that(all(t_weight_by_code_from$t_weight == 1))
+  sum_weights <- data_map %>%
+    dplyr::group_by({{ code_in }}, .add = TRUE) %>%
+    dplyr::summarise(t_weight = sum({{ weights }}))
+  assertthat::assert_that(all(sum_weights$t_weight == 1))
 
   # ---- transform data ----
-  data_out <- dplyr::right_join(x = data, y = code_dict, by = code_from) %>%
-    dplyr::mutate(dplyr::across({{ values_from }}, ~ .x * {{ weight_col }})) %>%
-    dplyr::group_by(.data[[code_to]], .add = TRUE) %>%
+  data_out <- data_map %>%
+    dplyr::mutate(dplyr::across({{ values_from }}, ~ .x * {{ weights }})) %>%
+    dplyr::group_by({{ code_out }}, .add = TRUE) %>%
     dplyr::summarise(dplyr::across({{ values_from }}, ~ sum(.x), .names = paste0("{.col}", names_suffix)))
 
   return(data_out)
--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -10,15 +10,20 @@ group_in <- conformr:::toy_AB$group_in
 code_dict <- conformr:::toy_AB$code_dict
 code_miss <- code_dict %>% dplyr::filter(code_A != "x4444")
 
+### data_map
+dm_in <- conformr:::toy_AB$data_in %>%
+  dplyr::right_join(code_dict, by = "code_A")
+
 ## ideal cases ----
 test_that("convert() output matches toy output", {
-  data_out_fnc <- conformr::convert(data = conformr:::toy_AB$group_in,
-                      code_dict = conformr:::toy_AB$code_dict,
-                      code_from = "code_A",
-                      code_to = "code_B",
+  data_out_fnc <- dm_in %>%
+    dplyr::group_by(country) %>%
+    conformr::convert(data_map = .,
+                      code_in = code_A,
+                      code_out = code_B,
                       values_from = starts_with("valA_"), # tidy-select
                       names_suffix = "_out",
-                      weight_col = weight)
+                      weights = weight)
 
   expect_identical(conformr:::toy_AB$data_out, data_out_fnc)
 })
