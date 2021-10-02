93450a5dfbd0a03b7eb6e308b458e2f04a4f51ef
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-10-02 19:57:32 +1000

    test: add NA value_from bad data maps
    - closes gh-24

--- tests/testthat/test-data-map-tools.R
+++ tests/testthat/test-data-map-tools.R
@@ -15,13 +15,63 @@ good_data_in <- conformr:::toy_AB$data_in %>%
 good_weights <- conformr:::toy_AB$weights_BA
 
 ## setup for bad inputs
-bad_dm <- list()
 bad_data_in <- list()
 bad_codes <- list()
 bad_weights <- list()
+bad_dm <- list()
+
+### bad data_in
+## [x] missing code_in in data_in
+bad_data_in$NA_code_in <- good_data_in %>%
+  dplyr::mutate(std_A = dplyr::case_when(std_A == "x3333" ~ NA_character_,
+                                         TRUE ~ std_A))
+bad_dm$value_no_code_in <- bad_data_in$NA_code_in %>%
+  dplyr::left_join(good_weights, by = "std_A")
+
+### bad codes/weights
+## [ ] extra codes with full join //
+## extra codes are not an issue? // drop rows without values?
+bad_codes$extra_stdB <- conformr:::toy_AB$codes_BA %>%
+  dplyr::add_row(std_B = "Z1", std_A = "x0987")
+bad_dm$extra_codes_no_data <- good_data_in %>%
+  dplyr::full_join(bad_codes$extra_stdB, by = "std_A")
+
+# [x] missing code-out for code-in
+bad_weights$drop_stdA <- good_weights %>%
+  dplyr::filter(std_A != "x2222")
+bad_dm$code_in_no_out <- good_data_in %>%
+  dplyr::full_join(bad_weights$drop_stdA, by = "std_A")
+
+# [x] weights more or less than 1
+bad_dm$weights <- good_dm %>%
+  dplyr::mutate(weight_more = dplyr::case_when(weight == 1 ~ weight,
+                                               TRUE ~ weight + 0.01),
+                weight_less = dplyr::case_when(
+                  weight == 0.25 ~ 0.2,
+                  TRUE ~ weight)
+  )
+
+### bad values
+bad_dm$codes_no_weights <-
+  good_dm %>%
+  select(-weight)
+
+bad_dm$one2many_NA_value <-
+  conformr:::toy_AB$data_map %>%
+  filter(n_dest > 1, country == "AUS") %>%
+  select(std_A, std_B, weight, A_100) %>%
+  mutate(A_100_NA = case_when(std_B %in% c("C8", "C9") ~ NA_real_,
+                              TRUE ~ A_100))
+
+bad_dm$oneMany2one_NA_value <-
+  conformr:::toy_AB$data_map %>%
+  filter(n_dest == 1, country == "AUS") %>%
+  select(std_A, std_B, weight, A_100) %>%
+  mutate(A_100_NA = case_when(std_A %in% c("x1111", "x3333") ~ NA_real_,
+                              TRUE ~ A_100))
 
 
-# data_map complete checks ----
+# data_map grouped checks ----
 test_that("grouped data map passes all checks", {
   group_pass_logical <-
     dm_needs_group %>%
@@ -38,8 +88,6 @@ test_that("grouped data map passes all checks", {
 )
 
 
-
-
 # dm_check_codes ----
 
 test_that("good data map passes dm_check_codes()",{
@@ -50,30 +98,11 @@ test_that("good data map passes dm_check_codes()",{
                    good_dm)
 })
 
-## missing code-out for code-in
-bad_weights$drop_stdA <- good_weights %>%
-  dplyr::filter(std_A != "x2222")
-bad_dm$code_in_no_out <- good_data_in %>%
-  dplyr::full_join(bad_weights$drop_stdA, by = "std_A")
-
-## missing code_in in data_in
-bad_data_in$NA_code <- good_data_in %>%
-  dplyr::mutate(std_A = dplyr::case_when(std_A == "x3333" ~ NA_character_,
-                                         TRUE ~ std_A))
-bad_dm$value_no_code_in <- bad_data_in$NA_code %>%
-  dplyr::left_join(good_weights, by = "std_A")
-
 test_that("missing codes raise error in dm_check_codes()", {
   expect_error(dm_check_codes(bad_dm$code_in_no_out, std_A, std_B))
   expect_error(dm_check_codes(bad_dm$value_no_code_in, std_A, std_B))
 })
 
-## extra codes are not an issue? // drop rows without values?
-bad_codes$extra_stdB <- conformr:::toy_AB$codes_BA %>%
-  dplyr::add_row(std_B = "Z1", std_A = "x0987")
-
-bad_dm$extra_codes_no_data <- good_data_in %>%
-  dplyr::full_join(bad_codes$extra_stdB, by = "std_A")
 
 # dm_check_weights ----
 
@@ -99,14 +128,10 @@ test_that("good data map passes dm_check_weights()", {
   expect_true(weights_pass_logical)
 })
 
-## bad weights
-bad_dm$weights <- good_dm %>%
-  dplyr::mutate(weight_more = dplyr::case_when(weight == 1 ~ weight,
-                                               TRUE ~ weight + 0.01),
-                weight_less = dplyr::case_when(
-                                       weight == 0.25 ~ 0.2,
-                                       TRUE ~ weight)
-                )
+test_that("missing codes raise error in dm_check_weights()", {
+  expect_error(dm_check_weights(bad_dm$code_in_no_out, std_A, std_B))
+  expect_error(dm_check_weights(bad_dm$value_no_code_in, std_A, std_B))
+})
 
 test_that("bad weights raise error in dm_check_weights()", {
   weights_fail_more <- bad_dm$weights %>%
@@ -130,10 +155,5 @@ test_that("bad weights raise error in dm_check_weights()", {
 }
 )
 
-
-
-
-
-
-
+# dm_check_values
 
