7121207fca7157776520add1fa58ba679c309a53
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2021-10-27 07:45:11 +1100

    feat(convert): Add toggle for data checks & sum NA
    Modifies convert() to allow for toggling:
      - sum_weight & comparison of total value (via .check=TRUE)
      - removal of NA when summarising by code_out (via .sum_na)

--- R/convert.R
+++ R/convert.R
@@ -25,19 +25,13 @@
 #' @examples
 #'
 #'
-convert <- function(data_map, code_in, code_out, weights, values_from, names_suffix = "_out"){
-  # ---- correspondence checks ---- //
-  ### value distribution weights total exactly 1 for each code_in (no value loss or creation)
-  sum_weights <- data_map %>%
-    dplyr::group_by({{ code_in }}, .add = TRUE) %>%
-    dplyr::summarise(t_weight = sum({{ weights }}))
-  assertthat::assert_that(all(sum_weights$t_weight == 1))
-
+convert <- function(data_map, code_in, code_out, weights, values_from,
+                    names_suffix = "_out", .check = TRUE, .sum_na = TRUE){
   # ---- transform data ----
   data_out <- data_map %>%
     dplyr::mutate(dplyr::across({{ values_from }}, ~ .x * {{ weights }})) %>%
     dplyr::group_by({{ code_out }}, .add = TRUE) %>%
-    dplyr::summarise(dplyr::across({{ values_from }}, ~ sum(.x)), .groups = "drop_last")
+    dplyr::summarise(dplyr::across({{ values_from }}, ~ sum(.x, na.rm = .sum_na)), .groups = "drop_last")
 
   # ---- check totals ----
   group <- data_map %>% dplyr::group_vars()
@@ -50,7 +44,17 @@ convert <- function(data_map, code_in, code_out, weights, values_from, names_suf
   # total_check <- dplyr::full_join(x = totals_in,
   #                                 y = totals_out,
   #                                 by = group, suffix = c(".in", ".out"))
-  assertthat::assert_that(assertthat::are_equal(totals_in, totals_out))
+
+  # ---- correspondence checks ----
+  ### value distribution weights total exactly 1 for each code_in (no value loss or creation)
+  sum_weights <- data_map %>%
+    dplyr::group_by({{ code_in }}, .add = TRUE) %>%
+    dplyr::summarise(t_weight = sum({{ weights }}))
+
+  if(.check == TRUE) {
+    assertthat::assert_that(assertthat::are_equal(totals_in, totals_out))
+    assertthat::assert_that(all(sum_weights$t_weight == 1))
+  }
 
   # ---- rename data_out ----
   data_out <- data_out %>%
