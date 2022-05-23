2a31349eda392687ad9383ed6b4eed6e750cc840
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-05-23 23:33:54 +1000

    test(pm_equal): organise reference tests
    Rename and combine existing tests for comparing:
      make_panel_map_equal() function output
      and reference panel_map for toy_AB case

--- R/make_panel_map_equal.R
+++ R/make_panel_map_equal.R
@@ -5,12 +5,14 @@
 #' @param code_dict Data frame containing correspondence between source and destination codes
 #' @param code_in Variable in `code_dict` containing source codes to convert from.
 #' @param code_out Variable in `code_dict` containing destination codes to convert to.
+#' @param .weights_to (optional) new column name for storing weights that will be applied to
+#' input values.
 #'
 #' @return Returns panel map
 #' @export
 #'
 #' @examples
-make_panel_map_equal <- function(code_dict, code_in, code_out, .split_in_name = NULL){
+make_panel_map_equal <- function(code_dict, code_in, code_out, .weights_to = NULL){
 
   ## get distinct correspondences
   code_dict <- code_dict %>%
@@ -19,12 +21,12 @@ make_panel_map_equal <- function(code_dict, code_in, code_out, .split_in_name = 
   ## code names as strings
 
   ## make column name for weights
-  .split_in_name <- .split_in_name %||% paste("split", deparse(substitute(code_in)), sep = "_")
+  .weights_to <- .weights_to %||% paste("split", deparse(substitute(code_in)), sep = "_")
 
   panel_map <- code_dict %>%
     dplyr::group_by({{code_in}}) %>%
     dplyr::mutate("n_dest" = dplyr::n(), ## faster than n_distinct()
-                  !!.split_in_name := 1 / n_dest) %>%
+                  !!.weights_to := 1 / n_dest) %>%
     dplyr::ungroup() %>%
     dplyr::select(-n_dest) # %>%
     # TODO: conformr::as_panel_map()
