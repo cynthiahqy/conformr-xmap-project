769012ed0eb4a381c279d6e6655c4ef5e737796e
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-05-23 23:31:45 +1000

    test: fix sum_greedy test NA_integer issue
    modify test to compare `NA_integer` instead of `NA_logical`
      to account for silent conversion by base::sum

--- tests/testthat/test-sum_greedy.R
+++ tests/testthat/test-sum_greedy.R
@@ -1,15 +1,17 @@
 test_that("sum of vector all NA returns NA", {
   vec_NA <- c(NA, NA, NA)
-  expect_equal(sum_NA(vec_NA), NA)
-  expect_equal(sum_NA(as.double(vec_NA)), NA)
+  # NA_integer required because sum() converts
+  # vec to numeric silently
+  expect_equal(sum_greedy(vec_NA), NA_integer_)
+  expect_equal(sum_greedy(as.double(vec_NA)), NA_integer_)
 })
 
 test_that("sum of mixed vector returns sum", {
   vec_mixed <- c(NA, 2, 6)
-  expect_equal(sum_NA(vec_mixed), 8)
+  expect_equal(sum_greedy(vec_mixed), 8)
 })
 
 test_that("sum of all numeric vector returns sum", {
   vec_value <- c(5, 10, 20)
-  expect_equal(sum_NA(vec_value), 35)
+  expect_equal(sum_greedy(vec_value), 35)
 })
