c42a55ab1210942ba42b5982ecb8c2151ac43039
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-05-25 11:55:43 +1000

    style: rename use_panel_map() argument
    Switch from `value_from` back to `values_from`

--- R/use_panel_map.R
+++ R/use_panel_map.R
@@ -34,7 +34,7 @@ transform_data_by_map <- function(map, data, values_from, code_from,
 #'
 #' @param map A data frame that contains a valid panel map for the transformation.
 #' @param data A data frame for transformation.
-#' @param value_from Variable in `data` containing the values to be converted.
+#' @param values_from Variable in `data` containing the values to be converted.
 #' @param from_code A character string of the variable containing the codes to convert from.
 #' Must be the same in `map` and `data`.
 #' @param to_code Variable in `map` containing the codes to convert to.
@@ -49,14 +49,14 @@ transform_data_by_map <- function(map, data, values_from, code_from,
 #' @export
 #'
 #' @examples
-use_panel_map <- function(map, data, value_from, from_code, to_code, weights,
+use_panel_map <- function(map, data, values_from, from_code, to_code, weights,
                           .suffix = NULL){
 
   # convert from_code to string
   from_code <- as_string(enexpr(from_code))
   # subset data for transformation
   data_in <- data %>%
-    dplyr::select(from_code, {{value_from}})
+    dplyr::select(dplyr::all_of(from_code), {{values_from}})
 
   # merge map and data // use default by= argument
   map_join_data <- dplyr::right_join(x = data_in,
@@ -65,14 +65,14 @@ use_panel_map <- function(map, data, value_from, from_code, to_code, weights,
 
   # apply transformation
   data_out <- map_join_data %>%
-    dplyr::mutate(dplyr::across({{ value_from }}, ~ .x * {{ weights }})) %>%
+    dplyr::mutate(dplyr::across({{ values_from }}, ~ .x * {{ weights }})) %>%
     dplyr::group_by({{ to_code }}, .add = TRUE) %>%
-    dplyr::summarise(dplyr::across({{ value_from }}, ~ conformr::sum_greedy(.x)), .groups = "drop_last")
+    dplyr::summarise(dplyr::across({{ values_from }}, ~ conformr::sum_greedy(.x)), .groups = "drop_last")
 
   # rename
   names_suffix <- .suffix %||% paste0("_", as_string(enexpr(to_code)))
   data_out <- data_out %>%
-    dplyr::rename_with(., ~ paste0(.x, names_suffix), .cols = {{ value_from }})
+    dplyr::rename_with(., ~ paste0(.x, names_suffix), .cols = {{ values_from }})
 
   return(data_out)
 }
