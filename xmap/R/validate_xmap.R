ab6bd52f4e858d66555a83b09e9592ed3632c5f1
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-12-22 23:12:40 +1100

    Add roxygen skeleton for validator function

---
+++ xmap/R/validate_xmap.R
@@ -0,0 +1,19 @@
+# Generated from create-xmap.Rmd: do not edit by hand
+
+#' A Validator for `xmap` objects
+#'
+#' @param x An `xmap` to be validated
+#'
+#' @return `x` if validation passes. Throws error otherwise.
+#' @export
+#'
+#' @examples
+validate_xmap <- function(x){
+  x <- unclass(x)
+  
+  ## type validation
+  
+  ## duplication and missingness checks
+  
+  ## complete weight check
+}
