46a55c4615aaeca550c33c13ed548a2b43ef42aa
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2023-01-11 14:20:40 +1100

    :package: render {xmap} v0.0.0.9004

--- xmap/R/validate_xmap.R
+++ xmap/R/validate_xmap.R
@@ -9,14 +9,12 @@
 #'
 #' @examples
 validate_xmap <- function(x){
-  xmap <- data.frame(x) # unclass(x)
-  src_col <- attr(x, "src_node")
-  tar_col <- attr(x, "tar_node")
-  weight_col <- attr(x, "link_weight")
-  col_attrs <- c(src_col, tar_col, weight_col)
+  df <- data.frame(x) # unclass(x)
+  xmap_attrs <- attributes(x)
+  col_attrs <- simplify2array(xmap_attrs[startsWith(names(xmap_attrs), "col")])
   
   ## dimension check
-  if (ncol(xmap) != 3) {
+  if (ncol(df) != 3) {
     rlang::abort(
       message = "`x` must only have the three columns: from, to, weights",
       class = "xmap_ncol"
@@ -24,16 +22,26 @@ validate_xmap <- function(x){
   }
 
   ## cols present
-  if (length(setdiff(names(xmap), col_attrs)) != 0){
-    rlang::abort(
-      message = "`src_node`, `tar_node` or `link_weight` column not found in `x`.",
+  missing_cols <- setdiff(names(df), col_attrs)
+  if (length(missing_cols) != 0){
+    cli::cli_abort(
+      message = "The column{?s} `{missing_cols}` {?was/were} not found in `x`.",
       class = "xmap_col_not_found"
     )
   }
 
   ## type validation
+  if (!is.numeric(df[, xmap_attrs$col_weights])) {
+    rlang::abort(
+      message = "`col_weights` column in `x` should be numeric",
+      class = "xmap_col_type"
+    )
+  }
+
+  ## from_set validation
+  # stopifnot(is.vector(from_set, mode = "character"))
 
   ## duplication and missingness checks
-  
+
   ## complete weight check
 }
--- xmap/tests/testthat/tests.R
+++ xmap/tests/testthat/tests.R
@@ -1,6 +1,31 @@
 # Generated from create-xmap.Rmd: do not edit by hand  
 testthat::test_that(
-  "validate_xmap() rejects df with too few/many columns",
+  "new_xmap() works as expected",
+  {
+    xmap <- new_xmap(x = int_dat$input_df, from = "node_A", to = "node_B", weights = "w_AB")
+    xmap_attrs <- attributes(xmap)
+    testthat::expect_s3_class(xmap, "xmap_df")
+    testthat::expect_identical(xmap_attrs$col_from, "node_A")
+    testthat::expect_identical(xmap_attrs$col_to, "node_B")
+    testthat::expect_identical(xmap_attrs$col_weights, "w_AB")
+    testthat::expect_identical(xmap_attrs$from_set, unique(int_dat$input_df$node_A))
+  }
+)
+
+testthat::test_that(
+  "new_xmap() rejects incorrect object types",
+  {
+    testthat::local_edition(3)
+    testthat::expect_snapshot(error = TRUE, new_xmap(x = "not a data.frame"))
+    testthat::expect_snapshot(error = TRUE, new_xmap(from = 33))
+    testthat::expect_snapshot(error = TRUE, new_xmap(from = "chr", to = expression(5 + 9)))
+    testthat::expect_snapshot(error = TRUE, new_xmap(from = "chr", to = "char", weights = c("dsf", "3925")))
+  }
+)
+
+## dimension check
+testthat::test_that(
+  "validate_xmap() rejects too few/many columns",
   {
     big_x <- data.frame(matrix(data = 1, nrow = 3, ncol = 4))
     little_x <- data.frame(matrix(data = 1, nrow = 3, ncol = 2))
@@ -10,8 +35,9 @@ testthat::test_that(
   }
 )
 
+## columns present
 testthat::test_that(
-  "validate_xmap() rejects df if named columns are not found",
+  "validate_xmap() rejects missing columns",
   {
     x <- new_xmap(int_dat$input_df,
                   from = "wrong_name",
@@ -19,6 +45,20 @@ testthat::test_that(
                   weights = "w_AB")
     testthat::expect_error(validate_xmap(x),
                            class = "xmap_col_not_found")
+  }
+)
+
+## column type
+testthat::test_that(
+  "validate_xmap() rejects non-numeric weight columns",
+  {
+    x <- new_xmap(int_dat$input_df,
+                  from = "node_A",
+                  to = "node_B",
+                  weights = "w_AB") |>
+      dplyr::mutate(w_AB = as.character(w_AB))
+    testthat::expect_error(validate_xmap(x),
+                           class = "xmap_col_type")
   }
 )
 
