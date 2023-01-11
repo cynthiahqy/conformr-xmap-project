070846d763c6438b6e5f63ffc6fecae25fbe875b
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2023-01-11 14:20:40 +1100

    :package: render {xmap} v0.0.0.9004

--- xmap/R/new_xmap.R
+++ xmap/R/new_xmap.R
@@ -3,14 +3,17 @@
 #' Low Level Constructor for xmap
 new_xmap <- function(x = data.frame(), from, to, weights) {
   stopifnot(is.data.frame(x))
-  stopifnot(is.character(from))
-  stopifnot(is.character(to))
-  stopifnot(is.character(weights))
+  stopifnot(length(from) == 1 && is.character(from))
+  stopifnot(length(to) == 1 && is.character(to))
+  stopifnot(length(weights) == 1 && is.character(weights))
   
   class(x) <- append("xmap_df", class(x))
 
+  from_set <- unique(x[[from]])
+
   structure(x, 
-            src_node = from,
-            tar_node = to,
-            link_weight = weights)
+            col_from = from,
+            col_to = to,
+            col_weights = weights,
+            from_set = from_set)
 }
