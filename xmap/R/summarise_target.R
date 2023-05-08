5bddb90b780b21604bc8206314646af08641dfb3
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2023-05-08 22:53:27 +1000

    :sparkle: improve summarise_target() glue handling
    add internal `englue_xmap()` to parse column names in glue
      formulas
    return table now retains original column names

--- xmap/R/summarise_target.R
+++ xmap/R/summarise_target.R
@@ -7,10 +7,10 @@
 #'
 #' @param links A data frame or `xmap_df`
 #' @inheritParams as_xmap
-#' @param composition_to A string specifying the new column to pass the summarised composition into. Default is "composition".
-#' @param frac_formula A formula specifying how to calculate the fraction of each
+#' @param composition_to A glue string specifying the new column to pass the summarised composition into. Default is "parts_{from}".
+#' @param frac_formula A glue formula specifying how to calculate the fraction of each
 #'   target value using the weighting variable. Default is "{from}*{weights}".
-#' @param unit_formula A formula specifying how to calculate the unit of each
+#' @param unit_formula A glue formula specifying how to calculate the unit of each
 #'   target value using the weighting variable. Default is "{from}".
 #' @param collapse A string specifying the separator to use when collapsing the
 #'   unit values. Default is "+".
@@ -35,30 +35,43 @@ summarise_target <- function(links, from, to, weights, composition_to, collapse,
 #' @importFrom glue glue_collapse
 summarise_target.data.frame <- function(
   links, from, to, weights,
-  composition_to = "composition",
+  composition_to = "parts_{from}",
   collapse = "+",
   frac_formula = "{from}*{weights}",
   unit_formula = "{from}"
 ) {
   ## rename
-  r_df <- links |>
-    dplyr::rename(from = {{from}},
-                  to = {{to}},
-                  weights = {{weights}})
+  # r_df <- links |>
+  #   dplyr::rename(from = {{from}},
+  #                 to = {{to}},
+  #                 weights = {{weights}})
+  
+  englue_xmap <- function(string_f, col_from, col_to, col_weights){
+    string_f |>   
+      gsub("from", col_from, x = _) |>
+      gsub("to", col_to, x = _) |>
+      gsub("weights", col_weights, x = _)
+  }
+  col_from <- rlang::englue("{{from}}")
+  col_to <- rlang::englue("{{to}}")
+  col_weights <- rlang::englue("{{weights}}")
+  frac_formula <- englue_xmap(frac_formula, col_from, col_to, col_weights)
+  unit_formula <- englue_xmap(unit_formula, col_from, col_to, col_weights)
+  comp_col <- englue_xmap(frac_formula, col_from, col_to, col_weights)
   ## summarise
-  r_df |>
+  links |>
     dplyr::mutate(part = dplyr::case_when(
-      weights == 1 ~ glue::glue(unit_formula),
-      weights < 1 ~ glue::glue(frac_formula))
+      {{weights}} == 1 ~ glue::glue(unit_formula),
+      {{weights}} < 1 ~ glue::glue(frac_formula))
     ) |>
-    dplyr::group_by(to) |>
-    dplyr::summarise(composition = glue::glue_collapse(part, collapse))
+    dplyr::group_by({{to}}) |>
+    dplyr::summarise("{comp_col}" := glue::glue_collapse(part, collapse))
 }
 
 #' @export
 #' @rdname summarise_target
 summarise_target.xmap_df <- function(links,
-                                     composition_to = "composition",
+                                     composition_to = "parts_{{from}}",
                                      collapse = "+",
                                      frac_formula = "{from}*{weights}",
                                      unit_formula = "{from}"
