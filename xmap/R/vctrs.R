699f2068a194574d9b40822e285df23a315a2812
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2024-07-01 10:34:43 +0200

    catch up commit on xmap

--- xmap/R/vctrs.R
+++ xmap/R/vctrs.R
@@ -1,5 +1,8 @@
 # Generated from create-xmap.Rmd: do not edit by hand
 
+# --- xmap_percent -----------------------------------------------------------
+
+## constructor
 new_percent <- function(x = double()) {
   if (!is_double(x)) {
     abort("`x` must be a double vector.")
@@ -7,6 +10,7 @@ new_percent <- function(x = double()) {
   vctrs::new_vctr(x, class = "xmap_percent")
 }
 
+## helpers
 percent <- function(x = double()) {
   x <- vctrs::vec_cast(x, double())
   new_percent(x)
@@ -16,6 +20,7 @@ is_percent <- function(x) {
   inherits(x, "xmap_percent")
 }
 
+## printing
 format.xmap_percent <- function(x, ...) {
   out <- formatC(signif(vctrs::vec_data(x) * 100, 3))
   out[is.na(x)] <- NA
@@ -26,3 +31,11 @@ format.xmap_percent <- function(x, ...) {
 vec_ptype_abbr.xmap_percent <- function(x, ...) {
   "pct"
 }
+
+## casting and coercion
+vec_ptype2.xmap_percent.double <- function(x, y, ...) double()
+vec_ptype2.double.xmap_percent <- function(x, y, ...) double()
+
+vec_cast.xmap_percent.xmap_percent <- function(x, to, ...) x
+vec_cast.xmap_percent.double <- function(x, to, ...) percent(x)
+vec_cast.double.xmap_percent <- function(x, to, ...) vec_data(x)
